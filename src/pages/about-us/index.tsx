7447208cfc9fe0bb3852483d76c8f4b9502905bb
joao-gallo <contato@kaueguedes.com.br>
2025-02-12 01:57:13 -0300

    update build metrics

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, MessageCircle } from "lucide-react";
 import Image from "next/image";
+
 export default function AboutUs() {
     return (
         <div className="m-4">
@@ -25,29 +26,37 @@ export default function AboutUs() {
                     Quem somos?
                 </h1>
                 <p className="text-sm text-muted-foreground mt-2">
-                A Epiroc é sua parceira para equipamentos de mineração e construção. Com origem na área de negócio Mining and Rock Excavation Technique e na divisão Hydraulic Attachment Tools da Atlas Copco, a Epiroc foi criada para ser um aliado mais forte para você. Ao aproveitar a experiência e o desempenho comprovados de diversas pessoas e um novo e ousado ímpeto para tornar o que é bom ainda melhor!<br/><br/>
+                    A Epiroc é sua parceira para equipamentos de mineração e construção. Com origem na área de negócio Mining and Rock Excavation Technique e na divisão Hydraulic Attachment Tools da Atlas Copco, a Epiroc foi criada para ser um aliado mais forte para você. Ao aproveitar a experiência e o desempenho comprovados de diversas pessoas e um novo e ousado ímpeto para tornar o que é bom ainda melhor!<br/><br/>
 
-Acreditamos na colaboração próxima com clientes, parceiros de negócios, colegas e outras partes interessadas. Nossa cultura de trabalho é voltada para o serviço e orientada para a ação, com uma forte devoção aos clientes.
+                    Acreditamos na colaboração próxima com clientes, parceiros de negócios, colegas e outras partes interessadas. Nossa cultura de trabalho é voltada para o serviço e orientada para a ação, com uma forte devoção aos clientes.
                 </p>
-                <Image className="mt-4 rounded-lg" src="https://epiroc.scene7.com/is/image/epiroc/Epiroc_B9391050-1?$landscape1600$&cropN=0.00,0.03,1.00,0.68" alt="About Us" width={screen.width} height={screen.height} />
+                <div className="relative w-full h-[200px] mt-4">
+                    <Image 
+                        src="https://epiroc.scene7.com/is/image/epiroc/Epiroc_B9391050-1" 
+                        alt="About Us" 
+                        fill
+                        className="object-cover rounded-lg"
+                    />
+                </div>
                 <h1 className="text-md font-bold mt-4">
                     Segurança & transparência
                 </h1>
                 <p className="text-sm text-muted-foreground mt-2">
-                Nosso compromisso é com a transparência, segurança e eficiência, garantindo que cada investidor tenha acesso a informações detalhadas e suporte contínuo. Contamos com um time de especialistas do mercado, sempre prontos para oferecer a melhor experiência de investimento.                </p>
-            <div className="mt-8">
-                <div>
-                    <h1 className="text-md font-bold text-center">
-                        Conheça mais sobre a Epiroc
-                    </h1>
-                    <Button className="w-full bg-brand mt-2">
-                        <a href="https://t.me/epirocglobal" className="flex items-center gap-2">
-                            <MessageCircle className="w-4 h-4" />
-                            Conheça mais sobre a Epiroc no Canal Oficial
-                        </a>
-                    </Button>
+                    Nosso compromisso é com a transparência, segurança e eficiência, garantindo que cada investidor tenha acesso a informações detalhadas e suporte contínuo. Contamos com um time de especialistas do mercado, sempre prontos para oferecer a melhor experiência de investimento.
+                </p>
+                <div className="mt-8">
+                    <div>
+                        <h1 className="text-md font-bold text-center">
+                            Conheça mais sobre a Epiroc
+                        </h1>
+                        <Button className="w-full bg-brand mt-2">
+                            <a href="https://t.me/epirocglobal" className="flex items-center gap-2">
+                                <MessageCircle className="w-4 h-4" />
+                                Conheça mais sobre a Epiroc no Canal Oficial
+                            </a>
+                        </Button>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
